0a2012e1708431f69ba90b2cce20f82def71b585
babudarla143 <148741953+babudarla143@users.noreply.github.com>
2024-10-24 11:41:33 +0530

    Add files via upload

---
+++ prg2.py
@@ -0,0 +1,16 @@
+class ractangle:
+    def__init__(self,x,y)
+    self.radus=x
+    self.wedth=y
+    
+    def area(self):
+        return = self.radus*self.wedth
+print('input here')
+x=int(input('enter the input'))
+y=int(input('enter the input'))
+r=ractangle(x,y)
+    
+    
+    
+    
+    
